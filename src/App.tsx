420eeacf3643d5c3df6bae84333451e5904859a4
Rakibul Hasan <rakibulhasan0641@gmail.com>
2024-07-06 10:25:48 +0600

    Initail Setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import MainLayout from './components/layout/MainLayout'
+
+function App() {
+
+
+  return (
+    <>
+       
+    <MainLayout/>
+       
+       
+    </>
+  )
+}
+
+export default App
